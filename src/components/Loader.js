291dafcff59ce14fce4b219cd82fb0663549be40
Charles Lee <charleslee1222@gmail.com>
2022-05-16 19:17:44 +0900

    version 1

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/loader.css'
+
+export const Loader = () => {
+  return (
+    <div className = "background">
+        <div className = "loadingText">
+            Every Day, Nearly 70,000 People Use the City Bike to Commute Around Seoul.
+            We analyzed usage data from all the bikes and stations accross Seoul to find out which factors have the biggest impact on Bike Usage
+        </div>
+    </div>
+  )
+}
